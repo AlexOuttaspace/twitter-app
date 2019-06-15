8b2900228d12413d36dc67b48ff052e0bb68103f
AlexOuttaspace <matanforeveryone@gmail.com>
2019-06-15 20:50:36 +0300

    redux init setup

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { styled } from 'linaria/react'
+import { Provider } from 'react-redux'
+
+import { store } from './store'
 
 const AppHeader = styled.header`
   border: 5px solid red;
@@ -7,8 +10,10 @@ const AppHeader = styled.header`
 
 export const App: React.FC = () => {
   return (
-    <div>
-      <AppHeader>hello</AppHeader>
-    </div>
+    <Provider store={store}>
+      <div>
+        <AppHeader>hello</AppHeader>
+      </div>
+    </Provider>
   )
 }
